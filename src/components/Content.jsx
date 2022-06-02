54ef715c0cf903f77404e080a7a463daaddb1ec1
Alex <3057ggwp@gmail.com>
2022-06-02 15:16:14 +0200

    initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile=()=>{
+    return<div className='content'>
+        <div>
+            <img src='https://pbs.twimg.com/media/EFIiRRyXYAA_xGw?format=jpg&name=900x900'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        My post
+        <div>
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+}
+export default Profile;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = ()=>{
+    return<header className='header'>
+        < img
+            src='https://cdn1.vectorstock.com/i/1000x1000/84/70/wings-heraldic-symbol-heraldic-coat-of-arms-vector-24328470.jpg'/>
+    </header>;
+
+}
+export default Header;
